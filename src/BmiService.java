d4b64d392f18ba64215e07727a60c88acd796f8d
Anfisa Golovanova <anfisa.faa@gmail.com>
2024-06-14 05:45:56 +0700

    Updated BmiService

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,14 +1,12 @@
 public class BmiService {
 
-    double weight;
-    double height;
-
-    double bmi;
+    // double weight;
+    // double height;
+    // double bmi;
 
     public int calculate(double weight, double height) {
 
-        bmi = weight / (height * height);
-
+        double bmi = weight / (height * height);
         return (int) bmi;
     }
 
